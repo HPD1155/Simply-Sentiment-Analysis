f5f4ec05dfc87f8c69791fb96c1876f6a2360df2
Aadi K <aadi_k@outlook.com>
2023-10-23 18:30:58 -0500

    First files

---
+++ main.py
@@ -0,0 +1,14 @@
+# Basic Sentiment Analysis Project | Aadi Kulkarni
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+
+# Load the dataset
+df = pd.read_csv('data.csv')
