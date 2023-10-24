4436621540c0359e51a7bf02da4b4fe43223b50b
Aadi K <aadi_k@outlook.com>
2023-10-23 20:23:13 -0500

    Second committ and probably final if not readme

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ model = keras.Sequential([
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the model on the training data
-history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, epochs=10)
